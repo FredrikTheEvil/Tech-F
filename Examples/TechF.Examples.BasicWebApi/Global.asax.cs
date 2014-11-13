b09c165cb58e10486bc34af93facc4917ad3cf6e
Fredrik A. Kristiansen <fredrik>
2014-11-13 20:56:19 +0100

    Added copyright notices

--- Examples/TechF.Examples.BasicWebApi/Global.asax.cs
+++ Examples/TechF.Examples.BasicWebApi/Global.asax.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿// Tech-F Framework
+// (C) Copyright 2014 Fredrik A.Kristiansen<fredrikaxk@gmail.com>
+//
+// All rights reserved.This program and the accompanying materials
+// are made available under the terms of the GNU Lesser General Public License
+// (LGPL) version 2.1 which accompanies this distribution, and is available at
+// http://www.gnu.org/licenses/lgpl-2.1.html
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU
+// Lesser General Public License for more details.
+//
+// Contributors:
+//     Fredrik A. Kristiansen<fredrikaxk@gmail.com>
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
